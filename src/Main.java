6f0574ee87d843f6699a1c7de54deb6c4daade48
Pittwo <malcolm.molloy@stu.ocsb.ca>
2022-10-18 14:38:27 -0400

    yeeesh
    almost finished the bonus

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,81 @@ public class Main {
 		// TODO Auto-generated method stub
 		Scanner in = new Scanner(System.in);
 		//Your challenge is to read in a price
+		System.out.println("price:");
+		double price = in.nextDouble();
 		//Read in the amount paid
+		System.out.println("amount paid:");
+		double paidAmount = in.nextDouble();
 		//Print out the amount of change
+		double change = paidAmount-price;
+		if (price >= paidAmount) {
+			System.out.println("Not Enough Money");
+		}
 		//Break the change into all denominations: 
-		//$100,$50,$20,$10,$5,$2,$1,$0.25,$0.10,$0.05
+		//$100
+		if (change / 100 >=1) {
+			int Hundreds = (int)(change/100);
+			System.out.println("# of 100's:"+Hundreds);
+			change = change-Hundreds*100;
+		}
+		//$50
+		if (change / 50 >=1) {
+			int Fiftys = (int)(change/50);
+			System.out.println("# of 50's:"+Fiftys);
+			change = change-Fiftys*50;
+		}
+		//$20
+		if (change / 20 >=1) {
+			int Twenties = (int)(change/20);
+			System.out.println("# of 20's:"+Twenties);
+			change = change-Twenties*20;
+		}
+		//$10
+		if (change / 10 >=1) {
+			int tens = (int)(change/10);
+			System.out.println("# of 10's:"+tens);
+			change = change-tens*10;
+		}
+		//$5
+		if (change / 5 >=1) {
+			int Fives = (int)(change/5);
+			System.out.println("# of 5's:"+Fives);
+			change = change-Fives*5;
+		}
+		//$2
+		if (change / 2 >=1) {
+			int twos = (int)(change/2);
+			System.out.println("# of Toonies's:"+twos);
+			change = change-twos*2;
+		}
+		//$1
+		if (change / 1 >=1) {
+			int ones = (int)(change/1);
+			System.out.println("# of Loonies:"+ones);
+			change = change-ones*1;
+		}
+		//$0.25
+		if (change / 0.25 >=1) {
+			int quarters = (int)(change/0.25);
+			System.out.println("# of Quarters:"+quarters);
+			change = change-quarters*0.25;
+		}
+		//$0.10
+		if (change / 0.10 >=1) {
+			int dimes = (int)(change/0.10);
+			System.out.println("# of Dimes:"+dimes);
+			change = change-dimes*0.10;
+			if (change >= 0.06 && change <=0.09) {
+				dimes = dimes+1;
+			}
+			change = 0;
+		}
+		//$0.05
+		if (change / 0.05 >=1) {
+			int Nickels = (int)(change/0.05);
+			System.out.println("# of Nickels:"+Nickels);
+			change = change-Nickels*0.05;
+		}
 		//As a bonus, make your project round to the nearest $0.05 like actual stores.
 		
 	}
